190ec0bcb12028d7a9d1b9ccfad2452db5156b1f
Kyubendo <kotigo111@gmail.com>
2020-09-03 17:01:24 +0300

    add http view

---
+++ src/Migrations/M200903000000CreateHttpView.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Wearesho\Yii\Guzzle\Migrations;
+
+use Yii;
+use yii\db\Migration;
+
+class M200903000000CreateHttpView extends Migration
+{
+    private const VIEW_NAME = 'http_view';
+    public function safeUp(): void
+    {
+        Yii::$app->db
+            ->createCommand(<<<SQL
+create view http_view as
+select req.uri,
+       req.method,
+       req.headers,
+       req.body,
+       res.status,
+       res.headers    as response_headers,
+       res.body       as response_body,
+       req.created_at as request_at,
+       res.created_at as response_at
+from http_request req
+         inner join http_response res on res.http_request_id = req.id
+order by req.id desc
+SQL
+            )
+            ->execute();
+        $this->createIndex(
+            'i_'.static::VIEW_NAME,
+            static::VIEW_NAME,
+            'uri'
+        );
+    }
+
+    public function safeDown(): void
+    {
+        Yii::$app->db
+            ->createCommand(
+                'drop view http_view;'
+            )
+            ->execute();
+    }
+}
