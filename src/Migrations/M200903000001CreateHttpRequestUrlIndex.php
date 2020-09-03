89fce83ee64ee5252a94482d21bc89490fd27675
Kyubendo <kotigo111@gmail.com>
2020-09-03 19:23:58 +0300

    fix formatting

--- src/Migrations/M200903000001CreateHttpRequestUrlIndex.php
+++ src/Migrations/M200903000001CreateHttpRequestUrlIndex.php
@@ -14,10 +14,7 @@ class M200903000001CreateHttpRequestUrlIndex extends db\Migration
     public function safeUp(): void
     {
         if ($this->db->driverName === 'mysql') {
-            $this->execute(/** @lang MySQL */
-                `CREATE INDEX i_http_request_uri
-                ON http_request (uri);`
-            );
+            $this->execute(`CREATE INDEX i_http_request_uri ON http_request (uri);`);
         } else {
             $this->createIndex(
                 'i_' . static::TABLE_NAME . '_uri',
